c580d2879ee542fbd4399cb20c73a93b4d9336eb
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-09-14 01:00:40 +0300

    feat: add new test in QuizList

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import classes from './QuizList.css';
 import {NavLink} from "react-router-dom";
-import axios from 'axios';
 import Loader from "../../components/UI/Loader/Loader";
+import axios from '../../axios/axios-quiz'
 
 export default class QuizList extends Component {
 
@@ -25,7 +25,7 @@ export default class QuizList extends Component {
 
   async componentDidMount() {
     try {
-      const response = await axios.get('https://react-quiz-be60e.firebaseio.com/quizes.json');
+      const response = await axios.get('quizes.json');
 
       const quizes = [];
       Object.keys(response.data).forEach((key, index) => {
