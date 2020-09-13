f36b94f618bf59575b92efe83e0771af796918d1
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-09-14 00:07:55 +0300

    feat: update tests list

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -5,22 +5,41 @@ import axios from 'axios';
 
 export default class QuizList extends Component {
 
+  state = {
+    quizes: []
+  }
+
   renderQuizes() {
-    return [1, 2, 3].map((quiz, index) => {
+    return this.state.quizes.map(quiz => {
       return (
-        <li key={index}>
-          <NavLink to={'/quiz/' + quiz} >
-            Тест {quiz}
+        <li key={quiz.id}>
+          <NavLink to={'/quiz/' + quiz.id} >
+            {quiz.name}
           </NavLink>
         </li>
       )
     })
   }
 
-  componentDidMount() {
-    axios.get('https://react-quiz-be60e.firebaseio.com/quiz.json').then(response => {
-      console.log(response)
-    })
+  async componentDidMount() {
+    try {
+      const response = await axios.get('https://react-quiz-be60e.firebaseio.com/quizes.json');
+
+      const quizes = [];
+      Object.keys(response.data).forEach((key, index) => {
+        quizes.push({
+          id: key,
+          name: `Test #${index + 1}`
+        })
+      })
+
+      this.setState({
+        quizes
+      })
+
+    } catch (e) {
+      console.log(e)
+    }
   }
 
   render() {
